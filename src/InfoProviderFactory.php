6d0b2730bd1e94e9ad4c8394bf9687c200c80009
Gossler Karoly <connor@connor.hu>
2026-01-30 12:00:46 +0100

    composer info, cleanup test info

--- src/InfoProviderFactory.php
+++ src/InfoProviderFactory.php
@@ -8,7 +8,7 @@ final class InfoProviderFactory
 {
 	public static function create(string $iban)
 	{
-		$iban = new Iban('HU28 1040 0212 5052 6666 7250 1014');
+		$iban = new Iban($iban);
 		
 		return match ($iban->countryCode()) {
 			'HU' => new InfoProviders\HU($iban),
